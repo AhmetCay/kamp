a0186a066d1f0388b148fea109a37ae32495cf2b
ahmetcay <ahmetcy@windowslive.com>
2015-07-30 22:55:22 +0300

    edit ve destroy işlemleri yapıldı

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all 
   end 
@@ -19,11 +20,21 @@ class TasksController < ApplicationController
   end
   def destroy
   	@task.destroy
-  	redirect_to tasks_path(@task), notice: 'Todo list was successfully destroyed.'
+  	redirect_to tasks_url, notice: 'Başarılı bir şekilde silindi.'
+  end
+  def update
+      if @task.update(task_params)
+        redirect_to tasks_path(@task), notice: 'Bilgiler güncellendi' 
+      else
+        render :edit 
+      end
   end
 
   private
   def task_params
   	params.require(:task).permit(:name)
   end
+  def set_task
+      @task = Task.find(params[:id])
+    end
 end
